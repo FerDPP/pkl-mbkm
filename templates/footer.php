a3d74989659ee2534f5d0c7015e2e6b17ff1c020
Muhammad Rizky Sydik <103189894+sydik45@users.noreply.github.com>
2024-01-30 17:53:34 +0700

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -7,7 +7,7 @@
 <footer class="user-select-none sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; StuntAssist 2023</span>
+            <span>Copyright &copy; SiPadis 2024</span>
         </div>
     </div>
 </footer>
